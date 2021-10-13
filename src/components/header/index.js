f11b3603c77c49c0fc39ec349be23ea3fed053fb
Yiteng Huang <hyy528924543@gmail.com>
2021-10-13 15:48:00 +1100

    designing login section

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
+// CSS Files
 import "./index.css";
+import "./login.css";
+
+// Image Files
 import img from "../../img/header_img.jpg";
 import logo from "../../img/header_logo.svg";
 
@@ -10,13 +15,57 @@ import FiberNewIcon from "@material-ui/icons/FiberNew";
 import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
 import PersonIcon from "@material-ui/icons/Person";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import CloseIcon from "@material-ui/icons/Close";
 
 function Header() {
   const [searchWord, setSearchWord] = useState("");
+  const [showLogin, setShowLogin] = useState(false);
 
   const searchCommunity = () => {
     console.log(searchWord);
   };
+
+  // Login Section
+  const Login = () => (
+    <div className="login">
+      <div className="login__container">
+        <div className="login__leftSection">
+          <h1>Join Our Community</h1>
+          <ul>
+            <li>Follow topics that matter to you</li>
+            <li>Connect with those who share your interests</li>
+            <li>Learn from the experts in our community</li>
+          </ul>
+          <p>Join now to ask, comment, and connect!</p>
+          <button>Join Now</button>
+        </div>
+        <div className="login__rightSection">
+          <h1>Log In</h1>
+          <CloseIcon
+            className="login__closeBtn"
+            onClick={() => setShowLogin(false)}
+          />
+          <div className="login__inputField login__username">
+            <label>Username or Email</label>
+            <input type="text" />
+          </div>
+          <div className="login__inputField login__password">
+            <label>Password</label>
+            <input type="password" />
+          </div>
+          <p>Forgot your password?</p>
+          <label>
+            <input type="checkbox" />
+            Stay logged in
+          </label>
+          <button>Log in</button>
+          <div>
+            This is the section await for Facebook / Google Authentication
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <div className="header">
@@ -43,13 +92,17 @@ function Header() {
           <Link to="/forums" className="header__link">
             <FormatListBulletedIcon className="header__icon" />
           </Link>
-          <div className="header__rightOptions__Login">
+          <div
+            className="header__rightOptions__Login"
+            onClick={() => setShowLogin(true)}
+          >
             <PersonIcon className="header__icon" />
             <p>Login / Join</p>
           </div>
           <MoreVertIcon className="header__icon" />
         </div>
       </div>
+      {showLogin ? <Login /> : null}
     </div>
   );
 }
